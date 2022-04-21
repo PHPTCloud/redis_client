301d2f1eddaf93d82e348843613a9befbbe8cd0f
Aleksey Yudov <tcloud.ax@gmail.com>
2022-04-21 13:24:15 -0700

    window

--- src/application.py
+++ src/application.py
@@ -11,30 +11,24 @@ class Application:
    
     def run(self, master=None):
         self.master = master
-
-        icon_path = self.styles.icon_path
-        main_bg = self.styles.main_bg
-        
-        start_window_size_x = self.styles.start_window_size_x
-        start_window_size_y = self.styles.start_window_size_y
         
         # get the screen dimension
         screen_width = master.winfo_screenwidth()
         screen_height = master.winfo_screenheight()
 
         # find the center point
-        center_x = int(screen_width / 2 - start_window_size_x / 2)
-        center_y = int(screen_height / 2 - start_window_size_y / 2)
-        start_window_size = f"{start_window_size_x}x{start_window_size_y}+{center_x}+{center_y}"
+        center_x = int(screen_width / 2 - self.styles.start_window_size_x / 2)
+        center_y = int(screen_height / 2 - self.styles.start_window_size_y / 2)
+        start_window_size = f"{self.styles.start_window_size_x}x{self.styles.start_window_size_y}+{center_x}+{center_y}"
         
         # Min size for window
         self.master.minsize(600, 300)
         # Initialize window icon
-        self.master.tk.call("wm", "iconphoto", self.master._w, tk.PhotoImage(file=icon_path))
+        self.master.tk.call("wm", "iconphoto", self.master._w, tk.PhotoImage(file=self.styles.icon_path))
         # Initialize window title
         self.master.title("Redis client")
         # Initialize frame settings
-        self.master['bg'] = main_bg
+        self.master['bg'] = self.styles.main_bg
         self.master.geometry(start_window_size)
         
         main_window = MainWindow(master=self.master, app=self)
--- src/windows.py
+++ src/windows.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from tkinter.ttk import Separator
+import tkinter as tk
 from PIL import Image, ImageTk
 from tkinter import font
 from src.styles import Styles
@@ -35,6 +35,32 @@ class MainWindow:
         self.create_connection_button.grid(column=0, row=1)
         
     def create_connection_click(self):
-        window = Toplevel(self.master)
+        window = CreateConnectionWindow(self.app, Toplevel())
+        window.init()
         
 
+class CreateConnectionWindow:
+    def __init__(self, app = None, master = None) -> None:
+        self.app = app
+        self.master = master
+        self.styles = Styles()
+        
+    def init(self):
+        start_window_size_x = 600
+        start_window_size_y = 300
+        
+        # get the screen dimension
+        screen_width = self.master.winfo_screenwidth()
+        screen_height = self.master.winfo_screenheight()
+
+        # find the center point
+        center_x = int(screen_width / 2 - start_window_size_x / 2)
+        center_y = int(screen_height / 2 - start_window_size_y / 2)
+        start_window_size = f"{start_window_size_x}x{start_window_size_y}+{center_x}+{center_y}"
+        self.master.minsize(600, 300)
+        self.master.tk.call("wm", "iconphoto", self.master._w, tk.PhotoImage(file=self.styles.icon_path))
+        # Initialize window title
+        self.master.title("Redis client: Create connections")
+        # Initialize frame settings
+        self.master['bg'] = self.styles.main_bg
+        self.master.geometry(start_window_size)
